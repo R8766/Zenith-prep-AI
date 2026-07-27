cfe1ae34a3bf9ce4800eff2e1ae8cd03be51cf39
R8766 <wakoderenu9@gmail.com>
2026-07-27 20:38:02 +0530

    Change login ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -78,7 +78,7 @@ function Login() {
 
         {/* Title */}
         <h1 className="text-3xl font-bold text-center mt-4 text-gray-900 dark:text-white">
-          PrepTracker
+          Zenith
         </h1>
 
         <p className="text-center text-gray-500 dark:text-gray-400 mt-2">
